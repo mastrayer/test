213495f547077e1e508b51bb6524969489f0047e
Jihwan Kyle Kim <mastrayer@naver.com>
2020-01-31 17:18:10 +0900

    Merge 2cf45d643789f488af15f2698446c7b764046b8a into cd07a8f5919ce862ed7d4a334ed8cd3b0aca6a27

---
+++ src/code.ts
@@ -0,0 +1,32 @@
+figma.showUI(__html__);
+
+figma.ui.onmessage = msg => {
+  if (msg.type === 'test') {
+    for (const node of figma.currentPage.selection) {
+      node
+        .exportAsync({
+          format: 'SVG',
+          contentsOnly: true,
+        })
+        .then(res => {
+          const svg = String.fromCharCode(...res);
+          console.log(svg);
+        });
+    }
+  }
+
+  // if (msg.type === 'create-rectangles') {
+  //   const nodes: SceneNode[] = [];
+  //   for (let i = 0; i < msg.count; i++) {
+  //     const rect = figma.createRectangle();
+  //     rect.x = i * 150;
+  //     rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
+  //     figma.currentPage.appendChild(rect);
+  //     nodes.push(rect);
+  //   }
+  //   figma.currentPage.selection = nodes;
+  //   figma.viewport.scrollAndZoomIntoView(nodes);
+  // }
+
+  figma.closePlugin();
+};
