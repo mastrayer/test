7e3667c31d0186808340f1bcb9c76f6d4e2e7331
mastrayer <mastrayer@naver.com>
2020-03-11 18:10:18 +0900

    v1.0.0-test

---
+++ rollup.config.js
@@ -0,0 +1,30 @@
+import commonjs from 'rollup-plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+import pkg from './package.json';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import typescriptPlugin from 'rollup-plugin-typescript2';
+
+process.env.BABEL_ENV = 'production';
+
+const extensions = ['.ts', '.tsx'];
+
+export default {
+  input: './src/index.ts',
+  plugins: [
+    peerDepsExternal(),
+    resolve({extensions}),
+    commonjs({include: 'node_modules/**'}),
+    typescriptPlugin({
+      useTsconfigDeclarationDir: true,
+      typescript: require('ttypescript')
+    }),
+    babel({ extensions, include: ['src/**/*'], runtimeHelpers: true }),
+  ],
+  output: [
+    {
+      file: pkg.module,
+      format: 'es',
+    }
+  ]
+};
