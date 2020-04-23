e8e0030ec6c220e9f337aba3e31a54f3745a1ada
Jihwan Kyle Kim <mastrayer@naver.com>
2020-04-23 10:48:32 +0900

    Update check-new-version.ts

--- scripts/check-new-version.ts
+++ scripts/check-new-version.ts
@@ -15,7 +15,7 @@ async function main() {
     // 기존에 다운받아둔 svgs 버전과 비교해서 이미 최신버전이면 다시 다운받지 않음
     const infoJson = fs.readFileSync(path.resolve(SVGS_PATH, 'info.json'), {encoding: 'utf8'});
     const info = JSON.parse(infoJson);
-    hasNewVersion = info.version.id === latestVersion.id;
+    hasNewVersion = info.version.id !== latestVersion.id;
   } catch {
     null;
   }
